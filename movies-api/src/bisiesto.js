6273cd032e54b1971e57ba5b046a0e435fe8e452
JLob Lap <joselobatof@gmail.com>
2021-01-13 19:45:18 -0600

    listado de años bisiestos

--- movies-api/src/bisiesto.js
+++ movies-api/src/bisiesto.js
@@ -1,4 +1,13 @@
 const bisiesto = (year) => {
+
+    // var bisiestos = [];
+    // for(let i = year; i >= 1580; i--){
+    //     if(i % 4 === 0 && (! i % 100 === 0 || i % 400 === 0)){
+    //         bisiestos.push(i);
+    //     }
+    // }
+    // return bisiestos;
+
     if(year % 4 === 0 && (! year % 100 === 0 || year % 400 === 0)){
         return `El año ${year} es bisiesto`;
     }else{
