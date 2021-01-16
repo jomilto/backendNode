c1fcabbfd10b578a0204db5529786637ff39a009
JLob Lap <joselobatof@gmail.com>
2021-01-15 20:40:11 -0600

    get route

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,21 +2,9 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesApi = require('./routes/movies');
 
-const { bisiesto } = require('./src/bisiesto');
-
-app.get('/', function(req,res){
-    res.send('Hello World');
-});
-
-app.get('/json', function(req,res){
-    res.json({data: 'Hello World'});
-});
-
-app.get('/bisiesto/:year', function(req,res){
-    const year = req.params.year;
-    res.send(bisiesto(year));
-});
+moviesApi(app);
 
 app.listen(config.port, () => {
     console.log(`Listening in http://localhost:${config.port}/`);
