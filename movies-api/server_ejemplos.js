c5b3445901310d0783c9ac342288d9e831d0cd72
JLob Lap <joselobatof@gmail.com>
2021-01-15 20:37:21 -0600

    server ejemplos

---
+++ movies-api/server_ejemplos.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+
+const { bisiesto } = require('./src/bisiesto');
+
+app.get('/', function(req,res){
+    res.send('Hello World');
+});
+
+app.get('/json', function(req,res){
+    res.json({data: 'Hello World'});
+});
+
+app.get('/bisiesto/:year', function(req,res){
+    const year = req.params.year;
+    res.send(bisiesto(year));
+});
+
+app.listen(config.port, () => {
+    console.log(`Listening in http://localhost:${config.port}/`);
+});
