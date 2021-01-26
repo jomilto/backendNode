2f040bcc5c6ce7baa70c3468b0fa60c10011a993
JLob Lap <joselobatof@gmail.com>
2021-01-25 20:07:50 -0600

    add boom to implement status and message errors

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,14 +4,21 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // middleware de body parser
 app.use(express.json());
 
+// routes
 moviesApi(app);
+// catch 404
+app.use(notFoundHandler);
 
+
+// Error middlewares
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
