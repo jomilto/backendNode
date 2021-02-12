de7f1b8001a138f46f25501e5b4ba85cac901619
JLob Lap <joselobatof@gmail.com>
2021-02-11 20:09:41 -0600

    getting token and using it to sign up a new user

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -1,17 +1,68 @@
 const express = require("express");
+const passport = require("passport");
+const boom = require("@hapi/boom");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
 
 const { config } = require("./config");
+
+const THIRTY_DAYS_IN_SEC = 2592000;
+
+const TWO_HOURS_IN_SEC = 7200;
 
 const app = express();
 
 // body parser
 app.use(express.json());
+app.use(cookieParser());
+
+// basic strategy
+require('./utils/auth/strategies/basic');
 
 app.post("/auth/sign-in", async function(req, res, next) {
 
+    const { rememberMe } = req.body;
+    passport.authenticate("basic", function(error, data){
+        try {
+            if(error || !data){
+                next(boom.unauthorized());
+            }
+
+            req.logIn(data, {session: false}, async function(error){
+                if(error) {
+                    next(error);
+                }
+
+                const { token, ...user } = data;
+
+                res.cookie("token", token, {
+                    httpOnly: !config.dev,
+                    secure: !config.dev,
+                    maxAge: rememberMe ? THIRTY_DAYS_IN_SEC: TWO_HOURS_IN_SEC
+                });
+
+                res.status(200).json(user);
+            })
+        } catch (error) {
+            next(error);
+        }
+    })(req, res, next);
 });
 
 app.post("/auth/sign-up", async function(req, res, next) {
+    const { body: user } = req;
+
+    try {
+        await axios({
+            url: `${config.apiUrl}/api/auth/sign-up`,
+            method: 'post',
+            data: user
+        });
+
+        res.status(201).json({message: 'user created'});
+    } catch (error) {
+        next(error);
+    }
 
 });
 
