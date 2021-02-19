2a6cddb40e853e6542361221417776973806b937
JLob Lap <joselobatof@gmail.com>
2021-02-18 19:30:07 -0600

    user oauth

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -18,6 +18,8 @@ app.use(cookieParser());
 
 // basic strategy
 require('./utils/auth/strategies/basic');
+// oauth strategy
+require('./utils/auth/strategies/oauth');
 
 app.post("/auth/sign-in", async function(req, res, next) {
 
@@ -113,6 +115,28 @@ app.delete("/user-movies/:userMovieId", async function(req, res, next) {
         next(error);
     }
 });
+
+app.get('/auth/google-oauth', passport.authenticate("google-oauth", {
+    scope: ['email', 'profile', 'openid']
+}));
+
+app.get('/auth/google-oauth/callback', 
+passport.authenticate('google-oauth', 
+{ session: false },
+function(req,res,next){
+    if(!req.user){
+        next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie('token', token, {
+        httpOnly: !config.dev,
+        secure: !config.dev
+    });
+
+    res.status(200).json(user);
+}));
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
